d6db37508bc23ddc49d5bd10e3eb66a34efd7709
tnhut <thanhnhut2506@gmail.com>
2019-01-10 23:20:53 +0700

    first

---
+++ NhutShop.Service/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhutShop.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ NhutShop.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NhutShop.Web.MvcApplication" Language="C#" %>
