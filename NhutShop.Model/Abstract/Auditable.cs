15f33af6ce52274c32297bee2eab37a6370c5b3b
tnhut <thanhnhut2506@gmail.com>
2019-01-12 23:35:49 +0700

    add class in model

---
+++ NhutShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhutShop.Model.Abstract
+{
+  public abstract  class Auditable: IAuditable
+    {
+             public  DateTime? CreatedDate { set; get; }
+             [MaxLength(270)]
+             public  string CreatedBy { set; get; }
+             public  DateTime? UpdatedDate { set; get; }
+             [MaxLength(270)]
+             public  string UpdatedBy { set; get; }
+
+             [MaxLength(270)]
+             public string MetaKeyword { set; get; }
+             [MaxLength(270)]
+             public string MetaDescription { set; get; }
+
+             public bool Status { set; get; }
+    }
+}
