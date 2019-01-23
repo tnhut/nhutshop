c955240a93e5809f462b65d1b383d5194bf9e32d
tnhut <thanhnhut2506@gmail.com>
2019-01-24 01:53:40 +0700

    Luyen tap angular

---
+++ NhutShop.Web/Scripts/Spa/app.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../Plugins/angular-1.7.6/angular.js" />
+
+var myApp = angular.module('myModule', []);
+myApp.controller("myController", myController); 
+
+myController.$inject = ['$scope'];
+function myController($scope) {
+    $scope.message = "HELLO YOU";
+}
+
