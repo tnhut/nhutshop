8b8d54297aa85e4c007a22e501f668c39fcac497
tnhut <thanhnhut2506@gmail.com>
2019-01-24 23:35:28 +0700

    Scope and RootScope, Range scope and rootscope

--- NhutShop.Web/Scripts/Spa/app.js
+++ NhutShop.Web/Scripts/Spa/app.js
@@ -1,10 +1,20 @@
 ﻿/// <reference path="../Plugins/angular-1.7.6/angular.js" />
 
 var myApp = angular.module('myModule', []);
-myApp.controller("myController", myController); 
-
+myApp.controller("studentmyController", studentmyController);
+myApp.controller("teachermyController", teachermyController);
+myApp.controller("schoolController", schoolController)
 myController.$inject = ['$scope'];
-function myController($scope) {
-    $scope.message = "HELLO YOU";
+
+function studentmyController($scope) {
+  //  $scope.message = "HELLO YOU";
+}
+
+function teachermyController($scope) {
+  //  $scope.message = "HELLO TEACHER";
+}
+
+function schoolController($scope) {
+    $scope.message = "ANNONCE EVERY ONE";
 }
 
