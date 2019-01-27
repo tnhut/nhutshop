e63281e2e55cd26864eaf311dec97fb317539462
tnhut <thanhnhut2506@gmail.com>
2019-01-28 01:06:57 +0700

    routing and design html for admin

--- NhutShop.Web/app/app.js
+++ NhutShop.Web/app/app.js
@@ -1,1 +1,15 @@
-﻿
+﻿/// <reference path="G:\LAPTOP\DUAN\Git\NhutShop.Web\Assets/Admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('nhutshop', ['nhutshop.products','nhutshop.common']).config(config);
+
+    config.$inject=['$stateProvider', '$urlRouterProvider']
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/Admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/Admin')
+    }
+})();
