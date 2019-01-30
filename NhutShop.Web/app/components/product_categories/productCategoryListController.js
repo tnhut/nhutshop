600683384729f258af7b258a6d703d4be42d3809
tnhut <thanhnhut2506@gmail.com>
2019-01-31 00:46:08 +0700

    paging in angularjs

--- NhutShop.Web/app/components/product_categories/productCategoryListController.js
+++ NhutShop.Web/app/components/product_categories/productCategoryListController.js
@@ -4,12 +4,23 @@
     productCategoryListController.$inject = ['$scope', 'apiService'];
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
-
+        $scope.page = 0;
+        $scope.pagesCount = 0;
         $scope.getProductCategories = getProductCategories;
 
-        function getProductCategories() {
-            apiService.get('/API/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data;
+        function getProductCategories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 2
+                }
+            }
+            apiService.get('/API/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
                 console.log('Load ProductCategory failed');
             });
