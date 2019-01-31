8505ade553c91936c1d74cb74d872c22faded011
tnhut <thanhnhut2506@gmail.com>
2019-01-31 23:16:34 +0700

    add notification service

--- NhutShop.Web/app/components/product_categories/productCategoryListController.js
+++ NhutShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,8 +1,8 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
-    function productCategoryListController($scope, apiService) {
+    productCategoryListController.$inject = ['$scope', 'apiService','notificationService'];
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
@@ -23,6 +23,12 @@
                 }
             }
             apiService.get('/API/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('Không tìm thấy bản ghi');
+                }
+                else {
+                    notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount+ ' bản ghi');
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
