ed6987fd4cdfe5e26a80aeb5c4065e530f01ce5f
tnhut <thanhnhut2506@gmail.com>
2019-02-28 02:00:14 +0700

    mange more image

--- NhutShop.Web/app/components/products/productAddController.js
+++ NhutShop.Web/app/components/products/productAddController.js
@@ -21,6 +21,7 @@
         }
 
         function AddProduct() {
+            $scope.product.MoreImages = JSON.stringify($scope.moreImages)
             apiService.post('/API/product/create', $scope.product, function (result) {
                 notificationService.displaySuccess(result.data.Name + 'đã thêm mới');
                 $state.go('products');
@@ -47,7 +48,19 @@
         $scope.ChooseImage = function () {
             var finder = new CKFinder();
             finder.selectActionFunction = function (fileUrl) {
-                $scope.product.Image = fileUrl;
+                $scope.$apply(function () {
+                    $scope.product.Image = fileUrl;
+                })             
+            }
+            finder.popup();
+        }
+        $scope.moreImages = [];
+        $scope.ChooseMoreImage = function () {
+            var finder = new CKFinder();
+            finder.selectActionFunction = function (fileUrl) {
+                $scope.$apply(function () {
+                    $scope.moreImages.push(fileUrl);
+                })              
             }
             finder.popup();
         }
--- NhutShop.Web/app/components/products/productEditController.js
+++ NhutShop.Web/app/components/products/productEditController.js
@@ -11,7 +11,7 @@
             height: '200px'
         }
         $scope.UpdateProduct = UpdateProduct;
-
+        $scope.moreImages = [];
         $scope.GetSeoTitle = GetSeoTitle;
 
         function GetSeoTitle() {
@@ -21,11 +21,13 @@
         function loadProductDetail() {
             apiService.get('/API/product/getbyid/' + $stateParams.id, null, function (result) {
                 $scope.product = result.data;
+                $scope.moreImages = JSON.parse($scope.product.MoreImages);
             }, function (error) {
                 notificationService.displayError(error.data);
             });
         }
         function UpdateProduct() {
+            $scope.product.MoreImages = JSON.stringify($scope.moreImages)
             apiService.post('/API/product/update', $scope.product, function (result) {
                 notificationService.displaySuccess(result.data.Name + 'đã được cập nhật');
                 $state.go('products');
@@ -52,10 +54,22 @@
         $scope.ChooseImage = function () {
             var finder = new CKFinder();
             finder.selectActionFunction = function (fileUrl) {
-                $scope.product.Image = fileUrl;
+                $scope.$apply(function () {
+                    $scope.product.Image = fileUrl;
+                })                
             }
             finder.popup();
         }
+
+        $scope.ChooseMoreImage = function () {
+            var finder = new CKFinder();
+            finder.selectActionFunction = function (fileUrl) {
+                $scope.$apply(function () {
+                    $scope.moreImages.push(fileUrl);
+                })
+            }
+            finder.popup();
+        }      
         loadProductCategory();
         loadProductDetail();
     }
